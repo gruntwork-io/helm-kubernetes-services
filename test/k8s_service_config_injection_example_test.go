d8739842819b4c0318d1a6387e1ed563f5bc13b8
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-03-08 17:27:47 -0800

    Stub for test

---
+++ test/k8s_service_config_injection_example_test.go
@@ -0,0 +1,14 @@
+// +build all integration
+
+// NOTE: We use build flags to differentiate between template tests and integration tests so that you can conveniently
+// run just the template tests. See the test README for more information.
+
+package test
+
+import (
+	"testing"
+)
+
+func TestK8SServiceConfigInjectionExample(t *testing.T) {
+	t.Fail()
+}
