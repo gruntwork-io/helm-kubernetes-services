ec4a89eb2c337484f3c3924359f6a7c042f9e050
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-03-10 22:52:33 -0700

    Tests for the config injection example

--- test/k8s_service_config_injection_example_test.go
+++ test/k8s_service_config_injection_example_test.go
@@ -6,9 +6,190 @@
 package test
 
 import (
+	"fmt"
+	"path/filepath"
+	"strings"
 	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
 )
 
-func TestK8SServiceConfigInjectionExample(t *testing.T) {
-	t.Fail()
+// Test the base case of the k8s-service-config-injection example, where the server port is set using hard coded
+// environment variables. This test will check that:
+//
+// 1. The docker container can be built
+// 2. The base values.yaml file can be used to deploy the docker container
+// 3. The deployed container responds to web requests with the default server text.
+func TestK8SServiceConfigInjectionBaseExample(t *testing.T) {
+	t.Parallel()
+
+	// Setup paths for testing the example chart
+	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
+	require.NoError(t, err)
+	examplePath, err := filepath.Abs(filepath.Join("..", "examples", "k8s-service-config-injection"))
+	require.NoError(t, err)
+
+	// Create a test namespace to deploy resources into, to avoid colliding with other tests
+	kubectlOptions := k8s.NewKubectlOptions("", "")
+	uniqueID := random.UniqueId()
+	testNamespace := fmt.Sprintf("k8s-service-config-injection-%s", strings.ToLower(uniqueID))
+	k8s.CreateNamespace(t, kubectlOptions, testNamespace)
+	defer k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
+	kubectlOptions.Namespace = testNamespace
+
+	// Build the docker image
+	createSampleAppDockerImage(t, uniqueID, examplePath)
+
+	// Install the base chart
+	// Set a random release name here so we can track it later
+	releaseName := fmt.Sprintf("k8s-service-config-injection-%s", strings.ToLower(uniqueID))
+	// Use the values file in the example and deploy the chart in the test namespace
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		ValuesFiles:    []string{filepath.Join(examplePath, "values.yaml")},
+		// Override the image tag
+		SetValues: map[string]string{"containerImage.tag": uniqueID},
+	}
+	defer helm.Delete(t, options, releaseName, true)
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	// Verify the app comes up cleanly and returns the expected text
+	expectedText := "Hello from backend"
+	validationFunction := sampleAppValidationFunctionGenerator(t, expectedText)
+	verifyPodsCreatedSuccessfully(t, kubectlOptions, "sample-sinatra-app", releaseName)
+	verifyAllPodsAvailable(t, kubectlOptions, "sample-sinatra-app", releaseName, validationFunction)
+	verifyServiceAvailable(t, kubectlOptions, "sample-sinatra-app", releaseName, validationFunction)
+
+}
+
+// Test the ConfigMap case of the k8s-service-config-injection example, where the server text is derived from a
+// ConfigMap that is injected as an environment variable. This test will check that:
+//
+// 1. The docker container can be built
+// 2. The provided kubernetes resource file can be used to create a ConfigMap containing a modified server text.
+// 3. The base values.yaml file can be combined with the extension for pulling in the server text from a config map.
+// 4. The deployed docker container responds with the server text derived from the config map
+func TestK8SServiceConfigInjectionConfigMapExample(t *testing.T) {
+	t.Parallel()
+
+	// Setup paths for testing the example chart
+	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
+	require.NoError(t, err)
+	examplePath, err := filepath.Abs(filepath.Join("..", "examples", "k8s-service-config-injection"))
+	require.NoError(t, err)
+
+	// Create a test namespace to deploy resources into, to avoid colliding with other tests
+	kubectlOptions := k8s.NewKubectlOptions("", "")
+	uniqueID := random.UniqueId()
+	testNamespace := fmt.Sprintf("k8s-service-config-injection-%s", strings.ToLower(uniqueID))
+	k8s.CreateNamespace(t, kubectlOptions, testNamespace)
+	defer k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
+	kubectlOptions.Namespace = testNamespace
+
+	// Build the docker image
+	createSampleAppDockerImage(t, uniqueID, examplePath)
+
+	// Install the configmap
+	kubeResourceConfigPath := filepath.Join(examplePath, "kubernetes", "config-map.yaml")
+	defer k8s.KubectlDelete(t, kubectlOptions, kubeResourceConfigPath)
+	k8s.KubectlApply(t, kubectlOptions, kubeResourceConfigPath)
+
+	// Install the chart with the base values and config map values
+	// Set a random release name here so we can track it later
+	releaseName := fmt.Sprintf("k8s-service-config-injection-%s", strings.ToLower(uniqueID))
+	// Use the values file in the example, override it with the configmap extension and deploy the chart in the test
+	// namespace
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		ValuesFiles: []string{
+			// Base example values
+			filepath.Join(examplePath, "values.yaml"),
+			// Example config map extensions values
+			filepath.Join(examplePath, "extensions", "config_map_values.yaml"),
+		},
+		// Override the image tag
+		SetValues: map[string]string{"containerImage.tag": uniqueID},
+	}
+	defer helm.Delete(t, options, releaseName, true)
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	// Verify the app comes up cleanly and returns the expected text
+	expectedText := "Hello! I was configured using a ConfigMap!"
+	validationFunction := sampleAppValidationFunctionGenerator(t, expectedText)
+	verifyPodsCreatedSuccessfully(t, kubectlOptions, "sample-sinatra-app", releaseName)
+	verifyAllPodsAvailable(t, kubectlOptions, "sample-sinatra-app", releaseName, validationFunction)
+	verifyServiceAvailable(t, kubectlOptions, "sample-sinatra-app", releaseName, validationFunction)
+}
+
+// Test the Secret case of the k8s-service-config-injection example, where the server text is derived from a
+// Secret that is injected as an environment variable. This test will check that:
+//
+// 1. The docker container can be built
+// 2. Create a Secret used to inject the server text.
+// 3. The base values.yaml file can be combined with the extension for pulling in the server text from a secret.
+// 4. The deployed docker container responds with the server text derived from the secret
+func TestK8SServiceConfigInjectionSecretExample(t *testing.T) {
+	t.Parallel()
+
+	// Setup paths for testing the example chart
+	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
+	require.NoError(t, err)
+	examplePath, err := filepath.Abs(filepath.Join("..", "examples", "k8s-service-config-injection"))
+	require.NoError(t, err)
+
+	// Create a test namespace to deploy resources into, to avoid colliding with other tests
+	kubectlOptions := k8s.NewKubectlOptions("", "")
+	uniqueID := random.UniqueId()
+	testNamespace := fmt.Sprintf("k8s-service-config-injection-%s", strings.ToLower(uniqueID))
+	k8s.CreateNamespace(t, kubectlOptions, testNamespace)
+	defer k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
+	kubectlOptions.Namespace = testNamespace
+
+	// Build the docker image
+	createSampleAppDockerImage(t, uniqueID, examplePath)
+
+	// Create a secret using kubectl
+	// Make sure to delete the secret in the undeploy process
+	defer k8s.RunKubectl(t, kubectlOptions, "delete", "secret", "sample-sinatra-app-server-text")
+	// Create Secret from a string literal
+	// kubectl create secret generic sample-sinatra-app-server-text --from-literal server_text='Hello! I was configured using a Secret!'
+	k8s.RunKubectl(
+		t,
+		kubectlOptions,
+		"create",
+		"secret",
+		"generic",
+		"sample-sinatra-app-server-text",
+		"--from-literal",
+		"server_text=Hello! I was configured using a Secret!",
+	)
+
+	// Install the chart with the base values and config map values
+	// Set a random release name here so we can track it later
+	releaseName := fmt.Sprintf("k8s-service-config-injection-%s", strings.ToLower(uniqueID))
+	// Use the values file in the example, override it with the secret extension and deploy the chart in the test
+	// namespace
+	options := &helm.Options{
+		KubectlOptions: kubectlOptions,
+		ValuesFiles: []string{
+			// Base example values
+			filepath.Join(examplePath, "values.yaml"),
+			// Example config map extensions values
+			filepath.Join(examplePath, "extensions", "secret_values.yaml"),
+		},
+		// Override the image tag
+		SetValues: map[string]string{"containerImage.tag": uniqueID},
+	}
+	defer helm.Delete(t, options, releaseName, true)
+	helm.Install(t, options, helmChartPath, releaseName)
+
+	// Verify the app comes up cleanly and returns the expected text
+	expectedText := "Hello! I was configured using a Secret!"
+	validationFunction := sampleAppValidationFunctionGenerator(t, expectedText)
+	verifyPodsCreatedSuccessfully(t, kubectlOptions, "sample-sinatra-app", releaseName)
+	verifyAllPodsAvailable(t, kubectlOptions, "sample-sinatra-app", releaseName, validationFunction)
+	verifyServiceAvailable(t, kubectlOptions, "sample-sinatra-app", releaseName, validationFunction)
 }
---
+++ test/k8s_service_example_test_helpers.go
@@ -0,0 +1,115 @@
+// +build all integration
+
+// NOTE: We use build flags to differentiate between template tests and integration tests so that you can conveniently
+// run just the template tests. See the test README for more information.
+
+package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	WaitTimerRetries = 60
+	WaitTimerSleep   = 5 * time.Second
+	NumPodsExpected  = 3
+)
+
+// verifyPodsCreatedSuccessfully waits until the pods for the given helm release are created.
+func verifyPodsCreatedSuccessfully(
+	t *testing.T,
+	kubectlOptions *k8s.KubectlOptions,
+	appName string,
+	releaseName string,
+) {
+	// Get the pods and wait until they are all ready
+	filters := metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s,app.kubernetes.io/instance=%s", appName, releaseName),
+	}
+	k8s.WaitUntilNumPodsCreated(t, kubectlOptions, filters, NumPodsExpected, WaitTimerRetries, WaitTimerSleep)
+	pods := k8s.ListPods(t, kubectlOptions, filters)
+	for _, pod := range pods {
+		k8s.WaitUntilPodAvailable(t, kubectlOptions, pod.Name, WaitTimerRetries, WaitTimerSleep)
+	}
+}
+
+// verifyAllPodsAvailable waits until all the pods from the release are up and ready to serve traffic. The
+// validationFunction is used to verify a successful response from the Pod.
+func verifyAllPodsAvailable(
+	t *testing.T,
+	kubectlOptions *k8s.KubectlOptions,
+	appName string,
+	releaseName string,
+	validationFunction func(int, string) bool,
+) {
+	filters := metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s,app.kubernetes.io/instance=%s", appName, releaseName),
+	}
+	pods := k8s.ListPods(t, kubectlOptions, filters)
+	for _, pod := range pods {
+		verifySinglePodAvailable(t, kubectlOptions, pod, validationFunction)
+	}
+}
+
+// verifySinglePodAvailable waits until the given pod is ready to serve traffic. Does so by pinging port 80 on the Pod
+// container. The validationFunction is used to verify a successful response from the Pod.
+func verifySinglePodAvailable(
+	t *testing.T,
+	kubectlOptions *k8s.KubectlOptions,
+	pod corev1.Pod,
+	validationFunction func(int, string) bool,
+) {
+	// Open a tunnel from any available port locally
+	localPort := k8s.GetAvailablePort(t)
+	tunnel := k8s.NewTunnel(kubectlOptions, k8s.ResourceTypePod, pod.Name, localPort, 80)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	// Try to access the service on the local port, retrying until we get a good response for up to 5 minutes
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		fmt.Sprintf("http://%s", tunnel.Endpoint()),
+		WaitTimerRetries,
+		WaitTimerSleep,
+		validationFunction,
+	)
+}
+
+// verifyServiceAvailable waits until the service associated with the helm release is available and ready to serve
+// traffic. The validationFunction is used to verify a successful response from the Pod.
+func verifyServiceAvailable(
+	t *testing.T,
+	kubectlOptions *k8s.KubectlOptions,
+	appName string,
+	releaseName string,
+	validationFunction func(int, string) bool,
+) {
+	// Get the service and wait until it is available
+	filters := metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s,app.kubernetes.io/instance=%s", appName, releaseName),
+	}
+	services := k8s.ListServices(t, kubectlOptions, filters)
+	require.Equal(t, len(services), 1)
+	service := services[0]
+	k8s.WaitUntilServiceAvailable(t, kubectlOptions, service.Name, WaitTimerRetries, WaitTimerSleep)
+
+	// Now hit the service endpoint to verify it is accessible
+	// Refresh service object in memory
+	service = *k8s.GetService(t, kubectlOptions, service.Name)
+	serviceEndpoint := k8s.GetServiceEndpoint(t, &service, 80)
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		fmt.Sprintf("http://%s", serviceEndpoint),
+		WaitTimerRetries,
+		WaitTimerSleep,
+		validationFunction,
+	)
+}
