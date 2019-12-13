077e20bfc6b9f92b5a5fe7fcc4cad70cc60eed7c
aechgg <hazg@me.com>
2019-12-13 09:17:17 +0000

    feat: issue-49 adding if statements for the cpu and memory with tests, changing to use include instead of template

--- test/k8s_service_horizontal_pod_autoscaler_template_test.go
+++ test/k8s_service_horizontal_pod_autoscaler_template_test.go
@@ -17,8 +17,8 @@ import (
 )
 
 // Test that setting horizontalPodAutoscaler.enabled = true will cause the helm template to render the Horizontal Pod
-// Autoscaler resource
-func TestK8SServiceHorizontalPodAutoscalerCreateTrueCreatesHorizontalPodAutoscaler(t *testing.T) {
+// Autoscaler resource with both metrics
+func TestK8SServiceHorizontalPodAutoscalerCreateTrueCreatesHorizontalPodAutoscalerWithAllMetrics(t *testing.T) {
 	t.Parallel()
 	minReplicas := "20"
 	maxReplicas := "30"
@@ -80,4 +80,111 @@ func TestK8SServiceHorizontalPodAutoscalerCreateFalse(t *testing.T) {
 	err = yaml.Unmarshal([]byte(out), &rendered)
 	assert.NoError(t, err)
 	assert.Equal(t, 0, len(rendered))
+}
+
+// Test that setting horizontalPodAutoscaler.enabled = true will cause the helm template to render the Horizontal Pod
+// Autoscaler resource with the cpu metric
+func TestK8SServiceHorizontalPodAutoscalerCreateTrueCreatesHorizontalPodAutoscalerWithCpuMetric(t *testing.T) {
+	t.Parallel()
+	minReplicas := "20"
+	maxReplicas := "30"
+	avgCpuUtil := "55"
+
+	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
+	require.NoError(t, err)
+
+	// We make sure to pass in the linter_values.yaml values file, which we assume has all the required values defined.
+	// We then use SetValues to override all the defaults.
+	options := &helm.Options{
+		ValuesFiles: []string{filepath.Join("..", "charts", "k8s-service", "linter_values.yaml")},
+		SetValues: map[string]string{
+			"horizontalPodAutoscaler.enabled":           "true",
+			"horizontalPodAutoscaler.minReplicas":       minReplicas,
+			"horizontalPodAutoscaler.maxReplicas":       maxReplicas,
+			"horizontalPodAutoscaler.avgCpuUtilization": avgCpuUtil,
+		},
+	}
+	out := helm.RenderTemplate(t, options, helmChartPath, []string{"templates/horizontalpodautoscaler.yaml"})
+
+	// We take the output and render it to a map to validate it has created a Horizontal Pod Autoscaler output or not
+	rendered := map[string]interface{}{}
+	err = yaml.Unmarshal([]byte(out), &rendered)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(rendered))
+	min, err := strconv.ParseFloat(minReplicas, 64)
+	max, err := strconv.ParseFloat(maxReplicas, 64)
+	avgCpu, err := strconv.ParseFloat(avgCpuUtil, 64)
+	assert.Equal(t, min, rendered["spec"].(map[string]interface{})["minReplicas"])
+	assert.Equal(t, max, rendered["spec"].(map[string]interface{})["maxReplicas"])
+	assert.Equal(t, avgCpu, rendered["spec"].(map[string]interface{})["metrics"].([]interface{})[0].(map[string]interface{})["resource"].(map[string]interface{})["target"].(map[string]interface{})["averageUtilization"])
+}
+
+// Test that setting horizontalPodAutoscaler.enabled = true will cause the helm template to render the Horizontal Pod
+// Autoscaler resource with the memory metric
+func TestK8SServiceHorizontalPodAutoscalerCreateTrueCreatesHorizontalPodAutoscalerWithMemoryMetric(t *testing.T) {
+	t.Parallel()
+	minReplicas := "20"
+	maxReplicas := "30"
+	avgMemoryUtil := "65"
+
+	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
+	require.NoError(t, err)
+
+	// We make sure to pass in the linter_values.yaml values file, which we assume has all the required values defined.
+	// We then use SetValues to override all the defaults.
+	options := &helm.Options{
+		ValuesFiles: []string{filepath.Join("..", "charts", "k8s-service", "linter_values.yaml")},
+		SetValues: map[string]string{
+			"horizontalPodAutoscaler.enabled":              "true",
+			"horizontalPodAutoscaler.minReplicas":          minReplicas,
+			"horizontalPodAutoscaler.maxReplicas":          maxReplicas,
+			"horizontalPodAutoscaler.avgMemoryUtilization": avgMemoryUtil,
+		},
+	}
+	out := helm.RenderTemplate(t, options, helmChartPath, []string{"templates/horizontalpodautoscaler.yaml"})
+
+	// We take the output and render it to a map to validate it has created a Horizontal Pod Autoscaler output or not
+	rendered := map[string]interface{}{}
+	err = yaml.Unmarshal([]byte(out), &rendered)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(rendered))
+	min, err := strconv.ParseFloat(minReplicas, 64)
+	max, err := strconv.ParseFloat(maxReplicas, 64)
+	avgMem, err := strconv.ParseFloat(avgMemoryUtil, 64)
+	assert.Equal(t, min, rendered["spec"].(map[string]interface{})["minReplicas"])
+	assert.Equal(t, max, rendered["spec"].(map[string]interface{})["maxReplicas"])
+	assert.Equal(t, avgMem, rendered["spec"].(map[string]interface{})["metrics"].([]interface{})[0].(map[string]interface{})["resource"].(map[string]interface{})["target"].(map[string]interface{})["averageUtilization"])
+}
+
+// Test that setting horizontalPodAutoscaler.enabled = true will cause the helm template to render the Horizontal Pod
+// Autoscaler resource with the no metrics
+func TestK8SServiceHorizontalPodAutoscalerCreateTrueCreatesHorizontalPodAutoscalerWithNoMetrics(t *testing.T) {
+	t.Parallel()
+	minReplicas := "20"
+	maxReplicas := "30"
+
+	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
+	require.NoError(t, err)
+
+	// We make sure to pass in the linter_values.yaml values file, which we assume has all the required values defined.
+	// We then use SetValues to override all the defaults.
+	options := &helm.Options{
+		ValuesFiles: []string{filepath.Join("..", "charts", "k8s-service", "linter_values.yaml")},
+		SetValues: map[string]string{
+			"horizontalPodAutoscaler.enabled":     "true",
+			"horizontalPodAutoscaler.minReplicas": minReplicas,
+			"horizontalPodAutoscaler.maxReplicas": maxReplicas,
+		},
+	}
+	out := helm.RenderTemplate(t, options, helmChartPath, []string{"templates/horizontalpodautoscaler.yaml"})
+
+	// We take the output and render it to a map to validate it has created a Horizontal Pod Autoscaler output or not
+	rendered := map[string]interface{}{}
+	err = yaml.Unmarshal([]byte(out), &rendered)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(rendered))
+	min, err := strconv.ParseFloat(minReplicas, 64)
+	max, err := strconv.ParseFloat(maxReplicas, 64)
+	assert.Equal(t, min, rendered["spec"].(map[string]interface{})["minReplicas"])
+	assert.Equal(t, max, rendered["spec"].(map[string]interface{})["maxReplicas"])
 }
