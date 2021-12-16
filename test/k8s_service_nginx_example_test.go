37c89b54be8c73baebcbbee9f243a72eb6939f44
yorinasub17 <430092+yorinasub17@users.noreply.github.com>
2021-12-16 09:33:34 -0600

    Introduce test stages

--- test/k8s_service_nginx_example_test.go
+++ test/k8s_service_nginx_example_test.go
@@ -1,3 +1,4 @@
+//go:build all || integration
 // +build all integration
 
 // NOTE: We use build flags to differentiate between template tests and integration tests so that you can conveniently
@@ -31,17 +32,42 @@ import (
 func TestK8SServiceNginxExample(t *testing.T) {
 	t.Parallel()
 
+	workingDir := filepath.Join(".", "stages", t.Name())
+
+	//os.Setenv("SKIP_setup", "true")
+	//os.Setenv("SKIP_create_namespace", "true")
+	//os.Setenv("SKIP_install", "true")
+	//os.Setenv("SKIP_validate_initial_deployment", "true")
+	//os.Setenv("SKIP_upgrade", "true")
+	//os.Setenv("SKIP_validate_upgrade", "true")
+	//os.Setenv("SKIP_delete", "true")
+	//os.Setenv("SKIP_delete_namespace", "true")
+
 	helmChartPath, err := filepath.Abs(filepath.Join("..", "charts", "k8s-service"))
 	require.NoError(t, err)
 	examplePath, err := filepath.Abs(filepath.Join("..", "examples", "k8s-service-nginx"))
 	require.NoError(t, err)
 
 	// Create a test namespace to deploy resources into, to avoid colliding with other tests
-	kubectlOptions := k8s.NewKubectlOptions("", "", "")
-	uniqueID := random.UniqueId()
+	test_structure.RunTestStage(t, "setup", func() {
+		kubectlOptions := k8s.NewKubectlOptions("", "", "")
+		test_structure.SaveKubectlOptions(t, workingDir, kubectlOptions)
+
+		uniqueID := random.UniqueId()
+		test_structure.SaveString(t, workingDir, "uniqueID", uniqueID)
+	})
+	kubectlOptions := test_structure.LoadKubectlOptions(t, workingDir)
+	uniqueID := test_structure.LoadString(t, workingDir, "uniqueID")
 	testNamespace := fmt.Sprintf("k8s-service-nginx-%s", strings.ToLower(uniqueID))
-	k8s.CreateNamespace(t, kubectlOptions, testNamespace)
-	defer k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
+
+	defer test_structure.RunTestStage(t, "delete_namespace", func() {
+		k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
+	})
+
+	test_structure.RunTestStage(t, "create_namespace", func() {
+		k8s.CreateNamespace(t, kubectlOptions, testNamespace)
+	})
+
 	kubectlOptions.Namespace = testNamespace
 
 	// Use the values file in the example and deploy the chart in the test namespace
@@ -67,33 +93,45 @@ func TestK8SServiceNginxExample(t *testing.T) {
 			"ingress.additionalPaths[1].servicePort":                               "80",
 		},
 	}
-	defer helm.Delete(t, options, releaseName, true)
-	helm.Install(t, options, helmChartPath, releaseName)
 
-	verifyPodsCreatedSuccessfully(t, kubectlOptions, "nginx", releaseName, NumPodsExpected)
-	verifyAllPodsAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
-	verifyServiceAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
+	defer test_structure.RunTestStage(t, "delete", func() {
+		helm.Delete(t, options, releaseName, true)
+	})
 
-	// We expect this to succeed, because the black hole service that overlaps with the nginx service is added as lower
-	// priority.
-	verifyIngressAvailable(t, kubectlOptions, "nginx", releaseName, "/app", nginxValidationFunction)
+	test_structure.RunTestStage(t, "install", func() {
+		helm.Install(t, options, helmChartPath, releaseName)
+	})
 
-	// On the other hand, we expect this to fail because the black hole service does not exist
-	verifyIngressAvailable(t, kubectlOptions, "nginx", releaseName, "/black-hole", serviceUnavailableValidationFunction)
+	test_structure.RunTestStage(t, "validate_initial_deployment", func() {
+		verifyPodsCreatedSuccessfully(t, kubectlOptions, "nginx", releaseName, NumPodsExpected)
+		verifyAllPodsAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
+		verifyServiceAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
 
-	// Now redeploy with higher priority path and make sure it fails
-	options.SetValues["ingress.additionalPathsHigherPriority[0].path"] = "/app"
-	options.SetValues["ingress.additionalPathsHigherPriority[0].serviceName"] = "black-hole"
-	options.SetValues["ingress.additionalPathsHigherPriority[0].servicePort"] = "80"
-	helm.Upgrade(t, options, helmChartPath, releaseName)
+		// We expect this to succeed, because the black hole service that overlaps with the nginx service is added as lower
+		// priority.
+		verifyIngressAvailable(t, kubectlOptions, "nginx", releaseName, "/app", nginxValidationFunction)
 
-	// We expect the service to still come up cleanly
-	verifyPodsCreatedSuccessfully(t, kubectlOptions, "nginx", releaseName, NumPodsExpected)
-	verifyAllPodsAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
-	verifyServiceAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
+		// On the other hand, we expect this to fail because the black hole service does not exist
+		verifyIngressAvailable(t, kubectlOptions, "nginx", releaseName, "/black-hole", serviceUnavailableValidationFunction)
+	})
 
-	// ... but now the nginx service via ingress should be unavailable because of the higher priority black hole path
-	verifyIngressAvailable(t, kubectlOptions, "nginx", releaseName, "/app", serviceUnavailableValidationFunction)
+	test_structure.RunTestStage(t, "upgrade", func() {
+		// Now redeploy with higher priority path and make sure it fails
+		options.SetValues["ingress.additionalPathsHigherPriority[0].path"] = "/app"
+		options.SetValues["ingress.additionalPathsHigherPriority[0].serviceName"] = "black-hole"
+		options.SetValues["ingress.additionalPathsHigherPriority[0].servicePort"] = "80"
+		helm.Upgrade(t, options, helmChartPath, releaseName)
+	})
+
+	test_structure.RunTestStage(t, "validate_upgrade", func() {
+		// We expect the service to still come up cleanly
+		verifyPodsCreatedSuccessfully(t, kubectlOptions, "nginx", releaseName, NumPodsExpected)
+		verifyAllPodsAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
+		verifyServiceAvailable(t, kubectlOptions, "nginx", releaseName, nginxValidationFunction)
+
+		// ... but now the nginx service via ingress should be unavailable because of the higher priority black hole path
+		verifyIngressAvailable(t, kubectlOptions, "nginx", releaseName, "/app", serviceUnavailableValidationFunction)
+	})
 }
 
 // nginxValidationFunction checks that we get a 200 response with the nginx welcome page.
