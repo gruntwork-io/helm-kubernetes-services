093fd2dabe03c9f3315ed1db0f3e218e7aac581b
nadiia-caspar <n.kotelnikova@goreha.com>
2023-08-18 16:05:00 +0200

    Update TestK8SServiceDeploymentCheckSecretStoreCSIBlock test

--- test/k8s_service_volume_secret_store_csi_template_test.go
+++ test/k8s_service_volume_secret_store_csi_template_test.go
@@ -7,14 +7,10 @@
 package test
 
 import (
-	// "fmt"
-	// "github.com/gruntwork-io/terratest/modules/random"
+	"testing"
 
-	// "github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	// "strings"
-	"testing"
 )
 
 func TestK8SServiceDeploymentCheckSecretStoreCSIBlock(t *testing.T) {
@@ -23,50 +19,35 @@ func TestK8SServiceDeploymentCheckSecretStoreCSIBlock(t *testing.T) {
 	deployment := renderK8SServiceDeploymentWithSetValues(
 		t,
 		map[string]string{
+			"secrets.dbsettings.as":        "csi",
+			"secrets.dbsettings.mountPath": "/etc/db",
 
-			"serviceAccount.name":   "secret-sa",
-			"secrets.dbsettings.as": "csi",
+			"secrets.dbsettings.csi.driver":              "secrets-store.csi.k8s.io",
+			"secrets.dbsettings.csi.readOnly":            "true",
+			"secrets.dbsettings.csi.secretProviderClass": "secret-provider-class",
 
-			"secrets.dbsettings.mountPath":    "/etc/db",
-			"secrets.dbsettings.csi.driver":   "secrets-store.csi.k8s.io",
-			"secrets.dbsettings.csi.readOnly": "true",
-
-			"secrets.dbsettings.csi.volumeAttributes.secretProviderClass": "secret-provider-class",
-
-			"secrets.dbsettings.items[0].name":                        "ENV_1",
-			"secrets.dbsettings.items[0].valueFrom.secretKeyRef.name": "dbsettings",
-			"secrets.dbsettings.items[0].valueFrom.secretKeyRef.key":  "ENV_1",
-			"secrets.dbsettings.items[1].name":                        "ENV_2",
-			"secrets.dbsettings.items[1].valueFrom.secretKeyRef.name": "dbsettings",
-			"secrets.dbsettings.items[1].valueFrom.secretKeyRef.key":  "ENV_2",
+			"secrets.dbsettings.items.host.envVarName": "DB_HOST",
+			"secrets.dbsettings.items.port.envVarName": "DB_PORT",
 		},
 	)
 
 	// Verify that there is only one container and only one volume
 	renderedPodContainers := deployment.Spec.Template.Spec.Containers
 	require.Equal(t, len(renderedPodContainers), 1)
-	appContainer := renderedPodContainers[0]
 	renderedPodVolumes := deployment.Spec.Template.Spec.Volumes
 	require.Equal(t, len(renderedPodVolumes), 1)
 	podVolume := renderedPodVolumes[0]
 
-	// Check that the pod volume is a secret volume
+	// Check that the pod volume has a correct name
 	assert.Equal(t, podVolume.Name, "dbsettings-volume")
-
+	
 	// Check that the pod volume has CSI block
-	require.NotNil(t, podVolume.CSI)
+	assert.NotNil(t, podVolume.CSI)
+
+	// Check that the pod volume has correct CSI driver and attributes
 	assert.Equal(t, podVolume.CSI.Driver, "secrets-store.csi.k8s.io")
 	assert.NotNil(t, podVolume.CSI.VolumeAttributes)
 	assert.Equal(t, podVolume.CSI.VolumeAttributes, map[string]string{
 		"secretProviderClass": "secret-provider-class",
 	})
-
-	// Check that the container env contains the ENV from secrets
-	require.NotNil(t, appContainer.Env)
-	assert.Equal(t, appContainer.Env[0].Name, "ENV_1")
-	assert.Equal(t, appContainer.Env[0].ValueFrom.SecretKeyRef.Name, "dbsettings")
-	assert.Equal(t, appContainer.Env[0].ValueFrom.SecretKeyRef.Key, "ENV_1")
-	assert.Equal(t, appContainer.Env[1].Name, "ENV_2")
-	assert.Equal(t, appContainer.Env[1].ValueFrom.SecretKeyRef.Name, "dbsettings")
-	assert.Equal(t, appContainer.Env[1].ValueFrom.SecretKeyRef.Key, "ENV_2")
 }
